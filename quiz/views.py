df827a8db6e87d0abc534977976bdcb676b1e633
Kamil Falkowski <falkowskikamil97@wp.pl>
2023-05-20 13:05:11 +0200

    Add login module

--- quiz/views.py
+++ quiz/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def main(request):
-    return render(request, template_name='quiz/main.html')
+    context = {}
+    context_message = request.GET.get('context')
+    if context_message:
+        context['message'] = context_message
+    return render(request, template_name='quiz/main.html', context=context)
--- user/views.py
+++ user/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.urls import reverse
 import logging
 logger = logging.getLogger(__name__)
 
@@ -23,8 +24,10 @@ def register(request):
             logger.error("New user")
         if not user_exist:
             user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password)
-            login(request, user)
-            return redirect('quiz:main')
+            login(request, user)      
+            context['message'] = 'Register Succesfully'
+            redirect_url = reverse('quiz:main') + '?context=' + context['message']
+            return redirect(redirect_url)
         else:
             context['message']='User alredy exist.'
             return render(request, 'user/register.html', context)
@@ -41,11 +44,16 @@ def login_request(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('quiz:main')
+            context['message'] = 'Login Succesfully'
+            redirect_url = reverse('quiz:main') + '?context=' + context['message']
+            return redirect(redirect_url)
         else:
             context['message'] = 'Invalid username or password'
             return render(request, 'user/login.html', context)
 
 def logout_request(request):
+    context={}
     logout(request)
-    return redirect('quiz:main')
+    context['message'] = 'Logout succesfully'
+    redirect_url = reverse('quiz:main') + '?context=' + context['message']
+    return redirect(redirect_url)
